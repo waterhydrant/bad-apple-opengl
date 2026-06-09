3c16244628e561d638cb9cfbb51f30de4eeb1fdf
waterhydrant <111168854+waterhydrant@users.noreply.github.com>
2026-06-09 12:22:00 -0700

    fix: Apple hardware compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,10 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     GLFWwindow *window =
         glfwCreateWindow(screenWidth, screenHeight, "Bad Apple", NULL, NULL);
     if (window == NULL) {
